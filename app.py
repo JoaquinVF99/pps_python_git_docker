0ab43338e092979c61336eeec96f617d83947336
Joaquin Vera Fenollar <4207474@alu.murciaeduca.es>
2024-04-20 18:12:01 +0200

    Línea Experto en WEB completa

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, jsonify
 from bayeta import frotar
+
+app = Flask(__name__)
+
+@app.route('/')
+def mostrar_frase_auspiciosa():
+    frase = frotar(1)[0]
+    return frase
+
+@app.route('/frotar/<int:n_frases>', methods=['GET'])
+def obtener_frases_auspiciosas(n_frases):
+    frases = frotar(n_frases)
+    return jsonify(frases)
+
+if __name__ == '__main__':
+    app.run()
+
--- bayeta.py
+++ bayeta.py
@@ -2,5 +2,6 @@ import random
 
 def frotar(n_frases: int = 1) -> list:
 
-    pass
+     frase_auspiciosa = "Que la suerte esté siempre de tu lado."  # Frase única
+     return [frase_auspiciosa] * n_frases  # Repetir la misma frase N veces
 
